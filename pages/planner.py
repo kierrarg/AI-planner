e2c98738620d7158033c80007ceda40cd334ad47
gagk3720 <kierra.gagnon@uleth.ca>
2023-09-07 11:33:47 -0600

    skeleton for planner

---
+++ pages/planner.py
@@ -0,0 +1,28 @@
+import datetime 
+import db
+from googleapiclient import build 
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request 
+
+# function to generate planner
+def generate_weekly_planner():
+    pass
+
+    # tasks = get_weekly_tasks()
+
+    # define time slots from 10am to 8pm
+
+    # assign tasks to time slots 
+    
+    # schedule tasks and create calendar event
+
+    # handle overlaps and conflicts
+
+    # remove completed tasks and google calendar event
+
+    # create study and assignment blocks based on due dates and keywords 
+
+# function to get tasks
+def get_weekly_tasks():
+    pass
+    # return tasks as a list
